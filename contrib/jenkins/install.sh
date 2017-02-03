dc5acc12109b003d15145fd19e33efe1d9a09318
Geoff Levand <geoff@infradead.org>
2017-02-03 14:43:17 -0800

    jenkins/install: Add config-file-provider plugin
    Signed-off-by: Geoff Levand <geoff@infradead.org>

--- contrib/jenkins/install.sh
+++ contrib/jenkins/install.sh
@@ -7,7 +7,7 @@ fi
 
 JENKINS_URL=$1
 
-for PLUGIN in git github rebuild parameterized-trigger copyartifact ssh-agent job-restrictions credentials-binding tap matrix-project; do
+for PLUGIN in git github rebuild parameterized-trigger copyartifact ssh-agent job-restrictions credentials-binding tap matrix-project config-file-provider; do
 	echo "Installing $PLUGIN..."
 	curl ${CURL_OPTS} --silent --header "Content-Type: application/xml" -XPOST "$JENKINS_URL/pluginManager/installNecessaryPlugins" --data "<install plugin=\"$PLUGIN@current\" />" >/dev/null
 done
